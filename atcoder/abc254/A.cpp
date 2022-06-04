4d7eb1939f766cf33b1aaa90b1bc2c792a467521
Amar Singh <amar.singh.cd.che20@itbhu.ac.in>
2022-06-04 17:45:00 +0530

    Add solution for problem `A - Last Two Digits` Link: https://atcoder.jp/contests/abc254/tasks/abc254_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc254/submissions/32214572

---
+++ atcoder/abc254/A.cpp
@@ -0,0 +1,17 @@
+/* Author- Amar Singh */
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int t, n, m, z;
+        cin >> t;
+    n = t % 10;
+    m = t/10;
+    z = m % 10;
+    cout << z << n;
+
+}
